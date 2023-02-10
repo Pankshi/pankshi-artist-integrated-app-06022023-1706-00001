a6420a3cd85e5d43b7f8354be500dc7e3604c7be
rahul <er.rahultiwari@gmail.com>
2023-02-10 19:36:33 +0530

    implementation of sing in module

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # pankshi-artist-integrated-app-06022023-1706-00001
+
+<!-- author = Rahul Tiwari building pankshi integrated andrios/ios app using wix api's-->
+
 pankshi artist app integrated to the wix pankshi website
+
+Need to install node.js in your machine to run the app
+
+Need to run below commands
---
+++ src/screens/SignInScreen.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  useWindowDimensions,
+} from "react-native";
+import Logo from "../../assets/images/Pankshi_logo.png";
+import CustomInput from "../components/CustomInput";
+import CustomButton from "../components/CustomButton";
+import { useState } from "react";
+
+const SignInScreen = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { height } = useWindowDimensions();
+
+  const onSignInPressed = () => {
+    console.warn("Sign in");
+  };
+
+  return (
+    <View style={styles.root}>
+      <Image
+        source={Logo}
+        style={[styles.logo, { height: height * 0.3 }]}
+        resizeMode="contain"
+      />
+      <CustomInput
+        placeholder="Username"
+        value={username}
+        setValue={setUsername}
+      />
+      <CustomInput
+        placeholder="password"
+        value={password}
+        setValue={setPassword}
+        secureTextEntry={true}
+      />
+      <CustomButton text="Sign In" onPress={onSignInPressed} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    alignItems: "center",
+    padding: 50,
+  },
+  logo: {
+    maxHeight: 200,
+    maxWidth: 300,
+    width: "70%",
+  },
+});
+
+export default SignInScreen;
